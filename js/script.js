d8bb7b21a51283362fb48326a8df756c1beabd3c
Rodrigo Castillo <rcastillo@hudsonhs.com>
2018-11-19 23:23:43 +0000

    rege

--- js/script.js
+++ js/script.js
@@ -9,3 +9,26 @@
 
 // DOCUMENT READY FUNCTION BELOW
 
+document.ready(function(){
+    
+    var userChoice = "";
+    var computerChoice = "";
+    var winner = ""; 
+    
+    var randomNumber = Math.random(); 
+    
+    var randomNumberGenerator =  Math.random() ;
+    
+    if (.75 < randomNumber){
+        var computerChoice = rock;
+    } else if (.35 < randomNumber < .75) {
+        var computerChoice = paper;
+    } else  {
+      var computerChoice = scissors; 
+    }
+    
+$('#button-1').click(function() {
+    $("#user").text( $("#c").val() )
+});
+
+})
